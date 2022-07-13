7abbcc1b737013ffc57c2b3e641ea0a5154eeb35
vikas pandey <vikaspandey@apptunix.com>
2022-07-13 09:59:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = ' MY ToDO-LIST ';
+  mylist: any[]=[];
+  addTask(item:any) {
+  this.mylist.push({ id:this.mylist.length, name:item })
+  }
+  deletetask(id: number) {
+    this.mylist = this.mylist.filter(item =>item.id!== id);
+   
+  }
 }
